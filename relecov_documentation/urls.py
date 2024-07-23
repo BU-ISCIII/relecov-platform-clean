54dff2ac9a88221b09bd3d85c5db70265d65bc6f
Shettland <pyblus@gmail.com>
2024-07-23 09:05:21 +0200

    Refactor relecov_documentation

--- relecov_documentation/urls.py
+++ relecov_documentation/urls.py
@@ -1,52 +1,55 @@
+# Generic imports
 from django.urls import path
 from django.conf import settings
-from relecov_documentation import views
 from django.conf.urls.static import static
 
+# Local imports
+import relecov_documentation
+
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("description", views.description, name="description"),
-    path("relecovInstall/", views.relecov_install, name="relecov_install"),
-    path("configuration/", views.configuration, name="configuration"),
-    path("metadata/", views.metadata, name="metadata"),
-    path("metadataLabExcel/", views.metadata_lab_excel, name="metadata_lab_excel"),
-    path("relecovTools/", views.relecov_tools, name="relecov_tools"),
-    path("intranetOverview/", views.intranet_overview, name="intranet_overview"),
+    path("", relecov_documentation.views.index, name="index"),
+    path("description", relecov_documentation.views.description, name="description"),
+    path("relecovInstall/", relecov_documentation.views.relecov_install, name="relecov_install"),
+    path("configuration/", relecov_documentation.views.configuration, name="configuration"),
+    path("metadata/", relecov_documentation.views.metadata, name="metadata"),
+    path("metadataLabExcel/", relecov_documentation.views.metadata_lab_excel, name="metadata_lab_excel"),
+    path("relecovTools/", relecov_documentation.views.relecov_tools, name="relecov_tools"),
+    path("intranetOverview/", relecov_documentation.views.intranet_overview, name="intranet_overview"),
     path(
         "intranetContactData/",
-        views.intranet_contact_data,
+        relecov_documentation.views.intranet_contact_data,
         name="intranet_contact_data",
     ),
     path(
         "intranetSampleSearch/",
-        views.intranet_sample_search,
+        relecov_documentation.views.intranet_sample_search,
         name="intranet_sample_search",
     ),
     path(
         "intranetReceivedSamples/",
-        views.intranet_received_samples,
+        relecov_documentation.views.intranet_received_samples,
         name="intranet_received_samples",
     ),
     path(
         "intranetUploadMetadata/",
-        views.intranet_upload_metadata,
+        relecov_documentation.views.intranet_upload_metadata,
         name="intranet_upload_metadata",
     ),
-    path("variantDashboard/", views.variant_dashboard, name="variant_dashboard"),
+    path("variantDashboard/", relecov_documentation.views.variant_dashboard, name="variant_dashboard"),
     path(
         "methodologyDashboard/",
-        views.methodology_dashboard,
+        relecov_documentation.views.methodology_dashboard,
         name="methodology_dashboard",
     ),
-    path("nextstrainInstall/", views.nextstrain_install, name="nextstrain_install"),
-    path("howtoNextstrain/", views.howto_nextstrain, name="howto_nextstrain"),
-    path("uploadToEna/", views.upload_to_ena, name="upload_to_ena"),
-    path("uploadToGisaid/", views.upload_to_gisaid, name="upload_to_gisaid"),
-    path("apiSchema/", views.api_schema, name="api_schema"),
-    path("howtoApi/", views.howto_api, name="howto_api"),
+    path("nextstrainInstall/", relecov_documentation.views.nextstrain_install, name="nextstrain_install"),
+    path("howtoNextstrain/", relecov_documentation.views.howto_nextstrain, name="howto_nextstrain"),
+    path("uploadToEna/", relecov_documentation.views.upload_to_ena, name="upload_to_ena"),
+    path("uploadToGisaid/", relecov_documentation.views.upload_to_gisaid, name="upload_to_gisaid"),
+    path("apiSchema/", relecov_documentation.views.api_schema, name="api_schema"),
+    path("howtoApi/", relecov_documentation.views.howto_api, name="howto_api"),
     path(
         "createNewUser/",
-        views.create_new_user,
+        relecov_documentation.views.create_new_user,
         name="create_new_user",
     ),
 ]
