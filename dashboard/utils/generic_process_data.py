e715ec9a81ba343dcd648210852c7eb90297c592
Shettland <pyblus@gmail.com>
2024-11-13 12:38:18 +0100

    Samples with not-provided num of samples in run are now skipped

--- dashboard/utils/generic_process_data.py
+++ dashboard/utils/generic_process_data.py
@@ -431,11 +431,15 @@ def pre_proc_depth_sample_run():
     for item in depth_sample_list:
         try:
             tmp_depth[item["sample__collecting_lab_sample_id"]] = float(item["value"])
-            int(item["number_of_samples_in_run"])
         except ValueError:
-            # ignore the entry if values cannot converted to float/int
+            # ignore the entry if value cannot converted to float
             continue
     for item in sample_in_run:
+        try:
+            int(item["number_of_samples_in_run"])
+        except ValueError:
+            # ignore the entry if number_of_samples cannot converted to int
+            continue
         if item["Sample name"] not in tmp_depth:
             continue
         d_value = tmp_depth[item["Sample name"]]
