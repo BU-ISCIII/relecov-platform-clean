ce62d3451c4f8e32531a0ebd683ee0bd597392bc
luissian <chapado.l@gmail.com>
2024-11-24 15:27:09 +0100

    remove the print statements

--- core/api/utils/common_functions.py
+++ core/api/utils/common_functions.py
@@ -6,8 +6,6 @@ import core.api.serializers
 def get_schema_version_if_exists(data):
     """Check if schema name and schema version exists"""
     apps_name = __package__.split(".")[0]
-    print("apps_name", apps_name)
-    print(data)
     if "schema_name" in data and "schema_version" in data:
         if core.models.Schema.objects.filter(
             schema_name__iexact=data["schema_name"],
