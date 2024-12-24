9199ace38645e019ec1c9b54ff8efcb0113e0fa2
Shettland <pyblus@gmail.com>
2024-12-24 11:55:12 +0100

    parsing received dates as datetime instead of str

--- core/utils/samples.py
+++ core/utils/samples.py
@@ -646,7 +646,7 @@ def get_all_recieved_samples_with_dates(accumulated=False):
         value = core.models.Sample.objects.filter(
             created_at__contains=date[0]
         ).count()
-        s_date = date[0].strftime("%Y %m %d")
+        s_date = date[0].date()
         if accumulated:
             sum += value
             r_samples.append({s_date: sum})
